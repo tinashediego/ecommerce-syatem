6430315e92122878097a4dbd0c92a7ce158a78f2
tinlee <tinashediego@gmail.com>
2020-07-29 15:37:38 +0200

    completed system

--- backend/data.js
+++ backend/data.js
@@ -1,31 +1,46 @@
-export default{
-products:[{
-    _id:'1',
-    name:'Slim shirt',
-    category:'shirts',
-    image:'/images/d1.jpg',
-    price:60,
-    brand:'Nike',
-    ratings:4.5,
-    numReviews:10
-},
-{
-    _id:'2',
-    name:'Fit shirt',
-    category:'shirts',
-    image:'/images/d1.jpg',
-    price:40,
-    brand:'Nike',
-    ratings:4.5,
-    numReviews:10
-},{
-    _id:'3',
-    name:'Slim pants',
-    category:'pants',
-    image:'/images/d1.jpg',
-    price:90,
-    brand:'Nike',
-    ratings:4.5,
-    numReviews:10
-}]
+export default {
+  products: [
+    {
+      _id: '1',
+      name: 'Slim Shirt',
+      category: 'Shirts',
+      image: '/images/d1.jpg',
+      price: 60,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 6,
+    },
+    {
+      _id: '2',
+      name: 'Fit Shirt',
+      category: 'Shirts',
+      image: '/images/d1.jpg',
+      price: 50,
+      brand: ' Nike',
+      rating: 4.2,
+      numReviews: 5
+    },
+    {
+      _id: '3',
+      name: 'Best Pants',
+      category: 'Pants',
+      image: '/images/d1.jpg',
+      price: 70,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 6,
+    }, {
+      _id: '4',
+      name: 'Best Pants',
+      category: 'Pants',
+      image: '/images/d1.jpg',
+      price: 70,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 6,
+    },
+  ]
 }
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,38 +1,90 @@
-import React, { useState , useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+import { listProducts } from '../actions/productActions';
+import Rating from '../components/Rating';
 
-
-function HomeScreen(props){
-  const [productsd , setProduct] = useState( [] );
-  
+function HomeScreen(props) {
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [sortOrder, setSortOrder] = useState('');
+  const category = props.match.params.id ? props.match.params.id : '';
+  const productList = useSelector((state) => state.productList);
+  const { products, loading, error } = productList;
+  const dispatch = useDispatch();
   useEffect(() => {
-    
-    const fetchData = async () => {
-      const {data} = await axios.get("/api/products");
-      setProduct(data);
-    }
+    dispatch(listProducts(category));
 
-    fetchData();
-  }, [])
+    return () => {
+      //
+    };
+  }, [category]);
 
-    return(
-        <div> <ul className="products">
-        {
-          productsd.map(product=>
-            <li>
-            <div className="product">
-                <Link to={'/product/'+product._id}><img className="product-image" src={product.image} alt="product"></img></Link>
-          <div className="product-name"><Link to={'/product/'+product._id}>{product.name}</Link></div>
-          <div className="brand-name">{product.brand}</div>
-          <div className="product-price">${product.price}</div>
-          <div className="product-rating">{product.ratings}</div>
-            </div>
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(listProducts(category, searchKeyword, sortOrder));
+  };
+  const sortHandler = (e) => {
+    setSortOrder(e.target.value);
+    dispatch(listProducts(category, searchKeyword, sortOrder));
+  };
+
+  return (
+    <>
+      {category && <h2>{category}</h2>}
+
+      <ul className="filter">
+        <li>
+          <form onSubmit={submitHandler}>
+            <input
+              name="searchKeyword"
+              onChange={(e) => setSearchKeyword(e.target.value)}
+            />
+            <button type="submit">Search</button>
+          </form>
         </li>
-          )
-        }
-    </ul></div>
-    )
+        <li>
+          Sort By{' '}
+          <select name="sortOrder" onChange={sortHandler}>
+            <option value="">Newest</option>
+            <option value="lowest">Lowest</option>
+            <option value="highest">Highest</option>
+          </select>
+        </li>
+      </ul>
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
+        <ul className="products">
+          {products.map((product) => (
+            <li key={product._id}>
+              <div className="product">
+                <Link to={'/product/' + product._id}>
+                  <img
+                    className="product-image"
+                    src={product.image}
+                    alt="product"
+                  />
+                </Link>
+                <div className="product-name">
+                  <Link to={'/product/' + product._id}>{product.name}</Link>
+                </div>
+                <div className="product-brand">{product.brand}</div>
+                <div className="product-price">${product.price}</div>
+                <div className="product-rating">
+                  <Rating
+                    value={product.rating}
+                    text={product.numReviews + ' reviews'}
+                  />
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
-
 export default HomeScreen;
